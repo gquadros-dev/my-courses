1030e790069fcebe58c4633946cabaaad5204b3d
GQuadros <gabrielfernand464508@gmail.com>
2023-05-02 22:23:46 -0300

    learning dart

--- Dart/first_dart_app/bin/first_dart_app.dart
+++ Dart/first_dart_app/bin/first_dart_app.dart
@@ -1,5 +1,13 @@
-import 'package:first_dart_app/first_dart_app.dart' as first_dart_app;
+void main() {
 
-void main(List<String> arguments) {
-  print('Hello world: ${first_dart_app.calculate()}!');
+  int age = 25; // poderia ser colocado como "var", assim vira tipagem dinâmica
+  double height = 1.76;
+  bool geek = true; // pode fazer comparações como "bool geek = (age == height)"
+  const String name = 'Gabriel Quadros';
+  const String nick = 'Gabe';
+
+  // List<String> namelist = ['Ricardo', 'Gabriel', 'Mauricio', 'Roberto', 'Evaldo'];
+  List<dynamic> gabriel = [age, height, geek, name, nick];
+
+  print('Hello world, my name is ${gabriel[3]}, my age is ${gabriel[0]}, my height is ${gabriel[1]} and am I geek? ${gabriel[2]}! And my nickname is ${gabriel[4]}.');
 }
